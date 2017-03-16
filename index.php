094780684a4560c92e31ecd2cdf5f75cb89a79b9
portiaherndon <portia.d.herndon@bison.howard.edu>
2017-03-16 02:21:59 +0000

    home page complete. just waiting on the links

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<title>NerdLuv
+	</title>
+	<head>
+		<img src="photos/nerdluv.png" alt="nerdluv logo" />
+		<p> where meek geeks meet </p> 
+		<br />
+	</head>
+	<body>
+		<p><strong> Welcome! </strong> </p>
+		<img src="photos/signup.gif" alt="singup gif" />
+		<br />
+		<img src="photos/heartbig.gif" alt="heart gif" />
+	</body>
+</html>
+<?php
+
+
+?>
+
