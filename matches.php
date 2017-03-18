8aedeb24e6ec66f865af26527d992cede5c8ca73
portiaherndon <portia.d.herndon@bison.howard.edu>
2017-03-18 02:25:46 +0000

    started matches page

---
+++ matches.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Matches
+		</title>
+		<img src="photos/nerdluv.png" alt="nerdluv logo" /><br>
+		where meek geeks meet 
+	
+	</head>
+	<body>
+		<form action = "matches-submit.php" method ="get">
+			<div>
+				Name: <input type="text" size="16" maxlength="16" name="name" /><br>
+				<input type="submit" value="View My Matches" />
+			</div>
+		</form>
+	</body>
+	<footer>
+		<p>This page is for single nerds to meet and date each other! Type in your personal information and wait for the nerdy luv to begin! Thank you for using our site. <br>
+		Results and page (C) Copyright NerdLuv Inc.
+		</p>
+		<a href="index.php">
+		<img src="back.gif" alt="homepage" title="homepage"/>
+		</a>
+	</footer>
+</html>
