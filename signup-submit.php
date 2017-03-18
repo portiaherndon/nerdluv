39a97684bd50c93b1367a6fed44c3f3d20cc0ddb
portiaherndon <portia.d.herndon@bison.howard.edu>
2017-03-18 01:39:15 +0000

    current status. moving progressively

--- signup-submit.php
+++ signup-submit.php
@@ -1,7 +1,12 @@
 <?DOCTYPE hmtl>
 <html>
+	<head>
+		<title> Sign Up
+		</title>
+		<img src="photos/nerdluv.png" alt=nerdluv logo" />
+	</head>
+
 	<?php 
-	echo var_dump($_POST);
 	if(!ctype_digit($_POST['age']))
 	{
 	?>
@@ -32,19 +37,41 @@
 		<p> Error: Max age must be larger than min age </p>
 	<?php
 	}
-	
-
+	elseif ((empty($_POST['name'])) || (empty($_POST['cc'])) || (empty($_POST['age'])) || (empty($_POST['person'])) || (empty($_POST['Favorite_OS'])) || (empty($_POST['min'])) || (empty($_POST['max'])))
+	{
+	?>
+		<p>One or more of the required fields have been left empty</p>
+	<?php
+	} 
 	else
 	{
 		$file = 'singles.txt';	
-		file_put_contents($file,$_POST['name'], FILE_APPEND | LOCK_EX);
+		$info1 = $_POST['name'];
+		$info2 = $_POST['cc'];
+		$info3 = $_POST['age'];
+		$info4 = $_POST['person'];
+		$info5 = $_POST['Favorite_OS'];
+		$info6 = $_POST['min'];
+		$info7 = $_POST['max'];
+		$info = array($info1,$info2,$info3,$info4,$info5,$info6,$info7);
+		file_put_contents($file,implode(',',$info)."\n", FILE_APPEND | LOCK_EX);
+	?>
+	<body>
+		<p><strong> Thank you! </strong><br><br>Welcome to NerdLuv, <?php echo $_POST['name'] ?>
 		
-		
-
-	
-					
+	</body>
+	<?php 
 	}
 	?>	
+	<footer>
+		<p>The page is for single nerds to meet and date each other! Type in your personal information and wait for the nerdy luv to begin! Thank you for using our site. Results and page (C) Copyright NerdLuv Inc. </p>
+		<a href="index.php">
+		<img src="back.gif" alt="homepage" title="homepage" />
+		</a>
+	</footer>
+	
+		
+	
 </html>
 
 
