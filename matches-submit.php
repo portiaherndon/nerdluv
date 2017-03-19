78455af486a864e462b6dec3b4de37ec0fc18705
portiaherndon <portia.d.herndon@bison.howard.edu>
2017-03-19 05:52:50 +0000

    when i try to save the min and max number into variables, they won't save. So if i try to save the current user's min and max, when i print it, I get 'n'

--- matches-submit.php
+++ matches-submit.php
@@ -30,25 +30,43 @@
 					$age = $elements[2];
 					$person = $elements[3];
 					$OS = $elements[4];
-					$min = elements[5];
+					$num1 = elements[5];
+					echo $num1;
+
 					$max = elements[6];
+					break;
 				}	
 			}
-			foreach ($lines as $line)
+			for($i=0;$i<count($lines);++$i)
 			{
-				$elements = explode(",",$line);
-				if($search !== elements[0])
-				{
+			
+				$element = explode(",",$lines[$i]);
 				
-				}					
-			} 
+				if($search !== $element[0])	
+				{
+					
+					if($sex !== $element[1]) 	
+					{
+			
+						if(($age >= $element[5]) && ($age <= $element[6]) && ($element[2] >= $num1))
+						{	
+							echo $lines[$i];
+							if($OS === $element[4])
+							{
+								echo $element[0]; ?><br><?php
+							} 
+						}
+					}
+				}
+			}
 		}
 		else
 		{
 		?>
 			<p>Error: User does not exist</p>
 		<?php
-		} 
+		}
+		
 		?>
 	</body>
 </html>
