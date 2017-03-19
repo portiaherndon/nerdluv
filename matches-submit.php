65ee949a4d4d25245e92fa003d6fbfb06339857e
portiaherndon <portia.d.herndon@bison.howard.edu>
2017-03-19 03:12:18 +0000

    current status: the current user's info saved into variables. to be compared to each user in the saved in the system.

--- matches-submit.php
+++ matches-submit.php
@@ -10,10 +10,44 @@
 		<?php
 		$file="singles.txt";
 		$search= $_GET['name'];
-		if(strpos(file_get_contents($file),$search)) 
+		if(empty($search))
 		{
-			?> <p> Matches for <?php echo $search; 
-			$lines = explode("\n",file_get_contents($file)); 
+		?>
+			<p>Error: Required field left empty</p>
+		<?php
+		}
+		elseif(strpos(file_get_contents($file),$search)) 
+		{
+			?> <p> Matches for <?php echo $search;
+			?><br><?php 
+			$lines = explode("\n",file_get_contents($file));
+			for($i=0; $i<count($lines); ++$i)
+			{
+				$elements = explode(",",$lines[$i]);
+				if($search === $elements[0])
+				{
+					$sex = $elements[1];
+					$age = $elements[2];
+					$person = $elements[3];
+					$OS = $elements[4];
+					$min = elements[5];
+					$max = elements[6];
+				}	
+			}
+			foreach ($lines as $line)
+			{
+				$elements = explode(",",$line);
+				if($search !== elements[0])
+				{
+				
+				}					
+			} 
+		}
+		else
+		{
+		?>
+			<p>Error: User does not exist</p>
+		<?php
 		} 
 		?>
 	</body>
